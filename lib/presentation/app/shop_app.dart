f8b75e550a492108d934988cf36bbbd5cdafe5e5
Vanilla <vetal-vetal-vv@mail.ru>
2023-01-29 18:22:28 +0300

    add app

---
+++ lib/presentation/app/shop_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ShopApp extends StatelessWidget {
+  const ShopApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //home: ShopListPage(),
+    );
+  }
+}
