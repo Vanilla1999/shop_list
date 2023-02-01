636c11da56875accae8dbb6792c1d210b97bb740
Vanilla <vetal-vetal-vv@mail.ru>
2023-02-01 18:02:59 +0300

    add simple splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:shop_list/presentation/app/shop_app.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const ShopApp());
 }
 
